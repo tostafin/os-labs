137cb5658fe17949a450ae523696e75e91f30ad7
Tomasz Ostafin <ostafintomasz1@gmail.com>
2022-03-28 09:27:32 +0200

    Fix cw03-zad1: forking only the parent process

--- cw03/zad1/main.c
+++ cw03/zad1/main.c
@@ -12,11 +12,13 @@ int main(int argc, char *argv[]) {
     if (argc != 2) raiseError("You must pass exactly one argument: number of child processes.");
     long n = strtol(argv[1], NULL, 10);
     if (n <= 0) raiseError("Incorrect input: you must pass a positive integer.");
-    
-    pid_t child_pid = -1;
-    while (child_pid != 0 && n--) child_pid = fork();
 
-    if (child_pid == 0) printf("I am a child process and my pid is %d.\n", getpid());
+    for (int i = 0; i < n; ++i) {
+        if (fork() == 0) {
+            printf("I am a child process and my pid is %d.\n", getpid());
+            exit(EXIT_SUCCESS);
+        }
+    }
 
     return 0;
 }
