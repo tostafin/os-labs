90b4b28402af74855413d3a30413007c64d5fadc
Tomasz Ostafin <ostafintomasz1@gmail.com>
2022-04-10 17:10:34 +0200

    Change cw05-zad3: now a number of producers doesn't depend on a variable inside consumer.c

--- cw05/zad3/consumer.c
+++ cw05/zad3/consumer.c
@@ -26,23 +26,32 @@ int main(int argc, char *argv[]) {
     char buff[N + 10];
     size_t len;
     int i;
-    int numOfProducers = 2;
-    char lines[numOfProducers][1000];
-    for (int j = 0; j < numOfProducers; ++j) lines[j][0] = '\0';
+    int numOfProducers = 0;
+    int maxNumOfProducers = 100;
+    char lines[maxNumOfProducers][1000];
+    for (int j = 0; j < maxNumOfProducers; ++j) lines[j][0] = '\n';
     while ((len = read(fifo, buff, N + 10)) > 0) {
         buff[len] = '\n';
         buff[len + 1] = '\0';
         char *content;
         i = (int) strtol(buff, &content, 10);
         ++content;
-        if (lines[i - 1][0] == '\0') strcpy(lines[i - 1], content);
+        if (lines[i - 1][0] == '\n') {
+            strcpy(lines[i - 1], content);
+            ++numOfProducers;
+        }
         else strcat(lines[i - 1], content);
     }
     close(fifo);
 
     FILE *file = fopen(argv[2], "w");
     if (file == NULL) raisePError("fopen");
-    for (int j = 0; j < numOfProducers; ++j) fwrite(lines[j], 1, strlen(lines[j]), file);
+    i = 0;
+    while (numOfProducers > 0) {
+        if (lines[i][0] != '\n') --numOfProducers;
+        fwrite(lines[i], 1, strlen(lines[i]), file);
+        ++i;
+    }
 
     fclose(file);
     return 0;
