3fb0bd41f49c7e8c79b482da5c6d0a49c0d8e99f
Tomasz Ostafin <ostafintomasz1@gmail.com>
2022-04-10 15:32:12 +0200

    Add cw05-zad3: version with one producent and one consumer

---
+++ cw05/zad3/consumer.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+void raiseError(char *message) {
+    fprintf(stderr, "%s\n", message);
+    exit(EXIT_FAILURE);
+}
+
+void raisePError(char *message) {
+    perror(message);
+    exit(EXIT_FAILURE);
+}
+
+// consumer pipePath filePath N
+int main(int argc, char *argv[]) {
+    if (argc != 4) raiseError("You must pass exactly 4 arguments.");
+    size_t N = (size_t) strtol(argv[3], NULL, 10);
+    if (N <= 0) raiseError("The last argument must be a positive integer.");
+
+    int fifo = open(argv[1], O_RDONLY);
+    if (fifo == -1) raisePError("open");
+
+    char buff[N + 10];
+    size_t len;
+    int i;
+    int numOfProducers = 1;
+    char lines[numOfProducers][1000];
+    for (int j = 0; j < numOfProducers; ++j) lines[j][0] = '\0';
+    while ((len = read(fifo, buff, N + 10)) > 0) {
+        buff[len] = '\0';
+        char *content;
+        i = (int) strtol(buff, &content, 10);
+        ++content;
+        if (lines[i - 1][0] == '\0') strcpy(lines[i - 1], content);
+        else strcat(lines[i - 1], content);
+    }
+    close(fifo);
+
+    FILE *file = fopen(argv[2], "w");
+    if (file == NULL) raisePError("fopen");
+    for (int j = 0; j < numOfProducers; ++j) {
+        size_t lineLen = strlen(lines[j]);
+        lines[j][lineLen] = '\n';
+        fwrite(lines[j], 1, lineLen, file);
+    }
+
+    fclose(file);
+    return 0;
+}
---
+++ cw05/zad3/producer.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <fcntl.h>
+#include <string.h>
+
+void raiseError(char *message) {
+    fprintf(stderr, "%s\n", message);
+    exit(EXIT_FAILURE);
+}
+
+void raisePError(char *message) {
+    perror(message);
+    exit(EXIT_FAILURE);
+}
+
+// producer pipePath i filePath N
+int main(int argc, char *argv[]) {
+    if (argc != 5) raiseError("You must pass exactly 4 arguments.");
+    size_t N = (size_t) strtol(argv[4], NULL, 10);
+    if (N <= 0) raiseError("The last argument must be a positive integer.");
+
+    int fifo = open(argv[1], O_WRONLY);
+    if (fifo == -1) raisePError("open");
+
+    FILE *file = fopen(argv[3], "r");
+    if (file == NULL) raisePError("fopen");
+    char buff[N + 10]; // the first few positions for i, then \n, then the actual content and '\0' at the end
+    strcpy(buff, argv[2]);
+    size_t iLen = strlen(argv[2]);
+    char *content = &buff[iLen]; // first index after i
+    *content = '\n';
+    ++content;
+    size_t len;
+    srand(time(NULL));
+    while ((len = fread(content, 1, N, file)) > 0) {
+        buff[iLen + 1 + len] = '\0';
+        sleep(rand() % 2 + 1);
+        write(fifo, buff, N + 10);
+        content = &buff[iLen] + 1;
+    }
+    close(fifo);
+    fclose(file);
+    return 0;
+}
